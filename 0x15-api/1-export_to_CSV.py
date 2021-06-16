0bf042c0a381496e275366ae28b7e8d671036752
kyle-gross <kyg555@outlook.com>
2021-06-16 19:56:22 +0000

    Add task 2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module saves employee task information to CSV"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    all_tasks = []
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
+    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                        .format(argv[1]))
+
+    name = user.json().get('name')
+    tasks = todo.json()
+
+    for task in tasks:
+        task_row = []
+        task_row.append(argv[1])
+        task_row.append(name)
+        task_row.append(task.get('completed'))
+        task_row.append(task.get('title'))
+
+        all_tasks.append(task_row)
+
+    filename = '2.csv'
+
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(all_tasks)
