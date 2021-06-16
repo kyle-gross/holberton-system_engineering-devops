86740d2f1c4efbc177179479ef8f3a57b1f634ee
kyle-gross <kyg555@outlook.com>
2021-06-16 21:00:01 +0000

    Add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module saves all employees task information to JSON"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    user_url = 'https://jsonplaceholder.typicode.com/users/'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    users = requests.get(user_url).json()
+    todos = requests.get(todo_url).json()
+
+    user_info = {}
+    users_and_tasks = {}
+
+    for user in users:
+        user_info[user['id']] = user['username']
+
+    for task in todos:
+        if users_and_tasks.get(task['userId'], False) is False:
+            users_and_tasks[task['userId']] = []
+        task_dict = {}
+        task_dict['task'] = task['title']
+        task_dict['username'] = user_info[task['userId']]
+        task_dict['completed'] = task['completed']
+
+        users_and_tasks[task['userId']].append(task_dict)
+
+    filename = 'todo_all_employees.json'
+
+    with open(filename, 'w') as jsonfile:
+        json.dump(users_and_tasks, jsonfile)
