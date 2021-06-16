0c213d0caf9adf48bc1a83da0104f9e7340583c7
kyle-gross <kyg555@outlook.com>
2021-06-16 19:21:16 +0000

    Add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module returns employee task information"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    completed_count = 0
+    task_list = []
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
+    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                        .format(argv[1]))
+
+    name = user.json().get('name')
+    tasks = todo.json()
+
+    for task in tasks:
+        if task.get('completed') is True:
+            completed_count += 1
+        task_list.append(task.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed_count, len(tasks)))
+
+    for task in task_list:
+        print('\t {}'.format(task))
